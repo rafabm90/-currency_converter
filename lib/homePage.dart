d92e41fbe32607312c9b7608a2b19c619f141674
rafabm90 <rafaelborges@arpiatecnologia.com>
2021-08-12 15:54:50 -0300

    Add controllers and functions to app

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,20 +1,195 @@
+import 'package:currency_converter/main.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key key }) : super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+  final bitcoinController = TextEditingController();
+
+  double dolar;
+  double euro;
+  double bitcoin;
+
+  void _realChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+    bitcoinController.text = (real / bitcoin).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+    bitcoinController.text = (dolar * this.dolar / bitcoin).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+    bitcoinController.text = (euro * this.euro / bitcoin).toStringAsFixed(2);
+  }
+
+  void _bitcoinChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double bitcoin = double.parse(text);
+    realController.text = (bitcoin * this.bitcoin).toStringAsFixed(2);
+    dolarController.text = (bitcoin * this.bitcoin / dolar).toStringAsFixed(2);
+    euroController.text = (bitcoin * this.bitcoin / euro).toStringAsFixed(2);
+  }
+
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+    bitcoinController.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         centerTitle: true,
         title: Text("Conversor de Moedas"),
+        backgroundColor: Colors.amber,
       ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carregando dados",
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro Carregando dados",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                bitcoin = snapshot.data["results"]["currencies"]["BTC"]["buy"];
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Icon(Icons.monetization_on,
+                            size: 150.0, color: Colors.amber),
+                        Divider(),
+                        CustomTextField(
+                          labelText: "Reais",
+                          prefixText: "R\$",
+                          controller: realController,
+                          onChanged: _realChanged,
+                        ),
+                        Divider(),
+                        CustomTextField(
+                          labelText: "Dolar",
+                          prefixText: "US\$",
+                          controller: dolarController,
+                          onChanged: _dolarChanged,
+                        ),
+                        Divider(),
+                        CustomTextField(
+                          labelText: "Euro",
+                          prefixText: "EUR\$",
+                          controller: euroController,
+                          onChanged: _euroChanged,
+                        ),
+                        Divider(),
+                        CustomTextField(
+                          labelText: "Bitcoin",
+                          prefixText: "BTC\$",
+                          controller: bitcoinController,
+                          onChanged: _bitcoinChanged,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+          }
+        },
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final String prefixText;
+  final TextEditingController controller;
+  final Function onChanged;
+
+  const CustomTextField(
+      {Key key,
+      this.labelText,
+      this.prefixText,
+      this.controller,
+      this.onChanged})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(
+          color: Colors.amber,
+        ),
+        border: OutlineInputBorder(),
+        prefixText: prefixText,
+      ),
+      style: TextStyle(
+        color: Colors.amber,
+        fontSize: 25.0,
+      ),
+      controller: controller,
+      onChanged: onChanged,
+      keyboardType: TextInputType.number,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:http/http.dart' as http;
 const request = "https://api.hgbrasil.com/finance?format=json&key=5479216c";
 
 Future<void> main() async {
-  
   runApp(MyApp());
 }
 
@@ -17,16 +16,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Currency Converter',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        ),
       ),
       home: HomePage(),
     );
   }
 }
 
-
 Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
-
 }
